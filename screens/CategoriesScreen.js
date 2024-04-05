ed65eede096ea2ade69f0d6e372a1d95d727ebab
manuelExtra <olaleyeemmanuel23@gmail.com>
2024-04-05 18:30:19 +0100

    added categories and meals component

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,38 @@
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import CategoryGridTile from '../components/CategoryGridTile';
+
+import { CATEGORIES } from '../data/dummy-data';
+
+function CategoriesScreen({ navigation }) {
+  function renderCategoryItem(item) {
+    function pressHandler() {
+      navigation.navigate('MealsOverview', {
+        categoryId: item.id,
+      });
+    }
+
+    return (
+      <CategoryGridTile
+        title={item.title}
+        color={item.color}
+        onPress={pressHandler}
+      />
+    );
+  }
+
+  return (
+    <View>
+      <Text>Category Screen</Text>
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => renderCategoryItem(item)}
+        numColumns={2}
+      />
+    </View>
+  );
+}
+
+export default CategoriesScreen;
+
+const styles = StyleSheet.create({});
